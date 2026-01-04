75f09efc74d95bb67668606f979d1b3e495f5794
CrisLap <99094086+CrisLap@users.noreply.github.com>
2026-01-04 19:25:16 +0100

    Refactor model loading in inference.py to dynamically fetch the latest sentiment model file from Hugging Face repository. Added a function to retrieve the latest model filename and updated the model download process to enhance flexibility and maintainability.

--- app/inference.py
+++ app/inference.py
@@ -1,15 +1,33 @@
 import fasttext
 import os
 from huggingface_hub import hf_hub_download
+from huggingface_hub import HfApi, hf_hub_download
 
-MODEL_REPO = "CrisLap/sentiment-model"
-MODEL_FILE = "sentiment_ft.bin"
+# Parametri
+REPO_ID = "CrisLap/sentiment-model"
+TOKEN = os.getenv("HF_TOKEN")
 
-token = os.getenv("HF_TOKEN")
-model_path = hf_hub_download(
-    repo_id="CrisLap/sentiment-model", filename="sentiment_ft.bin", token=token
-)
-model = fasttext.load_model(model_path)
+def get_latest_model_filename(repo_id, token):
+    api = HfApi()
+    # Recupera tutti i file nel repo
+    files = api.list_repo_files(repo_id=repo_id, token=token)
+    # Filtra quelli che iniziano con 'sentiment_ft' e finiscono con '.bin'
+    model_files = [f for f in files if f.startswith("sentiment_ft") and f.endswith(".bin")]
+    # Ordina e prendi l'ultimo (Hugging Face li restituisce solitamente in ordine)
+    return sorted(model_files)[-1] if model_files else None
+
+# Trova il nome dell'ultimo file
+latest_file = get_latest_model_filename(REPO_ID, TOKEN)
+
+if latest_file:
+    model_path = hf_hub_download(
+        repo_id=REPO_ID,
+        filename=latest_file,
+        token=TOKEN
+    )
+else:
+    raise FileNotFoundError("Nessun modello trovato nel repository!")
+
 
 
 def predict(text: str):
