6d38a894bbd5a2e9549f4cbdf4051f3a94be3b72
CrisLap <99094086+CrisLap@users.noreply.github.com>
2026-01-11 09:46:56 +0100

    Increase autotune duration in training script from 300 seconds to 600 seconds for improved parameter optimization.

--- training/train.py
+++ training/train.py
@@ -108,7 +108,7 @@ def train(epoch=25, lr=0.2, wordNgrams=2, dim=150):
             model = fasttext.train_supervised(
                 input=train_path,
                 autotuneValidationFile=test_path,
-                autotuneDuration=300,
+                autotuneDuration=600,
                 autotuneModelSize="100M",  # Force the model to weigh a maximum of 100MB
                 loss="ova",
             )
